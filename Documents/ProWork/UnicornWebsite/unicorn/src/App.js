1210aef8895008d3308625a9adaf8d1f136def3a
Joe Hoang <josephhoang97@gmail.com>
2019-06-06 11:53:58 +0100

    Save before ejecting

--- Documents/ProWork/UnicornWebsite/unicorn/src/App.js
+++ Documents/ProWork/UnicornWebsite/unicorn/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import AboutUs from './Pages/AboutUs/AboutUs'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navigation/>
+        </header>
+      
+       
+        <Route exact path='/' component={App} />
+        <Route path='/AboutUs' component={AboutUs} />
+
+      
+      </div>
+    </Router>
   );
 }
 
